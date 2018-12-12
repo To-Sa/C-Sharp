0492565b6ea6d01d5887ece49e075d54043e2b97
To-Sa <42317900+To-Sa@users.noreply.github.com>
2018-12-12 19:58:14 +0100

    Create LABEL or BUTTON COLOR CHANGE.cs

---
+++ DYNAMIC CREATE- MODIFY- DELETE.cs/LABEL or BUTTON COLOR CHANGE.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace PCC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 1st create some label in form or button
+
+            foreach (Label lab in this.Controls.OfType<Label>().Where(lab => lab.Name.StartsWith("label")))
+            {
+                if (lab.Name.StartsWith("label4"))
+                {
+                    lab.BackColor = Color.Red;
+                }
+                else
+                {
+                    lab.BackColor = Color.BlueViolet;
+                } 
+            }
+
+            foreach (Button lab in this.Controls.OfType<Button>().Where(lab => lab.Name.StartsWith("butt")))
+            {
+                lab.BackColor = Color.BlueViolet;
+            }
+        }       
+    }
+}
