61befb09baa8b715d8a403b5c006ced62d875a0d
To-Sa <42317900+To-Sa@users.noreply.github.com>
2019-01-12 02:40:29 +0100

    Update ALAPFOGALMAK.cs

--- ALAPFOGALMAK.cs
+++ ALAPFOGALMAK.cs
@@ -14,15 +14,17 @@ namespace PCC
         {
             //  MessageBox.Show("HELLO")  >>> SZINTAKTIKAI HIBA:  hiányzik a sorvégi ";".
             //                                SZEMANTIKAI HIBA:   Pl.: Prímszámokat kiíró program esetén kiírja az 1-et.
-            //  VÁLTOZÓ:        A változó a memóriában lefoglalt bitek azonosítója és tartalma.
-            //  DEKLARÁLÁS:     Egy változónévvel ellátott típust hozunk létre.
-            //  DEFINIÁLÁS:     Változónévvel ellátott típusnak értéket is adunk.
-            //  TÍPUSOK:        A C# erősen típusos nyelv. 
-                                /*A típus határozza meg, hogy egy változó milyen értékeket tartalmazhat illetve mekkora helyet foglal a memóriában.*/
-            //  PREFIX:         ++a; Először növeli az a változó értékét majd a többi részt.
-            //  POSTFIX:        a++; A postfix alaknál csak utólag adódik hozzá az a változóhoz az +1.
-            //  TÚLCSORDULÁS:   Akkor beszélünk túlcsordulásról, ha az adott érték már nem fér el a változónak fenntartott területen.
-            //  PRECEDENCIA:    Egy olyan rangsor, amelyet a nyelv követ és olyan sorrendben értelmezi a műveleteket.
+            //  VÁLTOZÓ:            A változó a memóriában lefoglalt bitek azonosítója és tartalma.
+            //  DEKLARÁLÁS:         Egy változónévvel ellátott típust hozunk létre.
+            //  DEFINIÁLÁS:         Változónévvel ellátott típusnak értéket is adunk.
+            //  TÍPUSOK:            A C# erősen típusos nyelv. 
+                                    /*A típus határozza meg, hogy egy változó milyen értékeket tartalmazhat illetve mekkora helyet foglal a memóriában.*/
+            //  PREFIX:             ++a; Először növeli az a változó értékét majd a többi részt.
+            //  POSTFIX:            a++; A postfix alaknál csak utólag adódik hozzá az a változóhoz az +1.
+            //  TÚLCSORDULÁS:       Akkor beszélünk túlcsordulásról, ha az adott érték már nem fér el a változónak fenntartott területen.
+            //  PRECEDENCIA:        Egy olyan rangsor, amelyet a nyelv követ és olyan sorrendben értelmezi a műveleteket.
+            //  RÖVIDÍTETT VERZIÓ:  int szam1 = 7;
+            //                      szam1 *= -5; MessageBox.Show(Convert.ToString(szam1));
         }
     }
 }
