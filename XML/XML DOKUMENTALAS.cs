637854c9a76fce4a64bd8f11a123edb1d5fb00ae
To-Sa <42317900+To-Sa@users.noreply.github.com>
2019-01-19 14:53:09 +0100

    Create XML DOKUMENTALAS.cs

---
+++ XML/XML DOKUMENTALAS.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Windows.Forms;
+
+namespace PCC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool okNok = ConvBinDec("v", out int vmi);
+            MessageBox.Show(okNok.ToString());
+        }
+        /// <summary> Ez a függvény átkonvertálja a kapott stringet vagy bináris számot decimális számmá. </summary>  
+        /// <param name="bin">Bináris szám. </param> 
+        /// <param name="dec">Ha sikerül a konvertálás, ide kerül az átalakított szám. </param>
+        /// <returns> Ha sikerült a konvertálás true, ha nem false értékkel tér vissza </returns>
+        /// <exception cref="System.OverflowException"/> 
+        static bool ConvBinDec(string bin, out int dec)
+        {
+            dec = 0;
+            for (int i = 0; i < bin.Length; i++)
+            {
+                if (bin[i] == '1')
+                {
+                    dec += (int)Math.Pow(2, bin.Length - 1 - i);
+                }
+                else if (bin[i] != '0') dec = 0;
+                return false;
+            }
+            return true;
+        }
+    }
+}
