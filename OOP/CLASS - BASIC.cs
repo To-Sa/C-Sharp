79aa521c6a95e75fad7c1980d65731c06c25f142
To-Sa <42317900+To-Sa@users.noreply.github.com>
2019-01-20 18:16:27 +0100

    Create CLASS - BASIC.cs

---
+++ OOP/CLASS - BASIC.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Windows.Forms;
+
+namespace PCC
+{
+    class person //CLASS >>> REF TYPE
+    {
+        public string name;
+        public int age;
+        public string address;
+        public string job;
+
+        public void WR()
+        {
+            MessageBox.Show("NAME: " + name);
+            MessageBox.Show("AGE: " + age);
+            MessageBox.Show("ADDRESS: " + address);
+            MessageBox.Show("JOB: " + job);
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            person ts = new person();
+            ts.name = "TOM";
+            ts.age = 32;
+            ts.address = "NY";
+            ts.job = "XYZ Plc.";
+            person e2 = ts; e2.name = "Joe";
+            ts.WR();
+            e2.WR();
+        }
+    }
+}
